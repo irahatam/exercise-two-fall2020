7973a6f75692a5d0fd6f1d914c6d218ffa0ee1cf
Matahari Kinanti Herwin <matahari@nyu.edu>
2020-09-29 14:53:02 -0400

    basic setup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
+
+import Home from "./containers/Home";
 
 function App() {
   return (
-    <div className="App">
+    <Router>
+      <Switch>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
 
-    </div>
   );
 }
 
