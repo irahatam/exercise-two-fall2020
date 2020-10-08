b62f91a215fef40e7c50fe71d963f85a2efe66e3
Matahari Kinanti Herwin <matahari@nyu.edu>
2020-10-08 00:54:00 -0400

    added things from oct 1 & oct 6 class

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,14 +4,14 @@ function Header() {
     return (
         <header className="Header"> 
             <div>
-                <h1> Header Title </h1>
+                <h1> Today's Weather </h1>
             </div>
             <nav>
-                <a href="#"> Jakarta </a>
-                <a href="#"> Sydney </a>
-                <a href="#"> Seattle </a>
-                <a href="#"> Tokyo </a>
-                <a href="#"> London </a>
+                <a href="/?city=Jakarta"> Jakarta </a>
+                <a href="/?city=Sydney"> Sydney </a>
+                <a href="/?city=Seattle"> Seattle </a>
+                <a href="/?city=Tokyo"> Tokyo </a>
+                <a href="/?city=London"> London </a>
             </nav>
         </header>
     );
--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,37 +1,118 @@
-import React from "react";
+import React, {  useState, useEffect, useMemo  } from 'react';
+import axios from 'axios';
+import {  useHistory  } from 'react-router-dom';
 
-import Header from "../components/Header";
+import Header from '../components/Header';
+import WeatherImage from "../components/WeatherImage";
 
 const weatherKey = `c192e7c68ea0a788e86845763d2aa2ed`;
 
 function Home() {
-    /* 
-    DISPLAYS:
-    Weather Type (ex. Cloudy)
-    Current Temperature
-    High Temperature
-    Low Temperature
-    Cloudiness
-    Humidity
-    Wind Speed
-    */
-    return(
-        <>
+    const history = useHistory();
+
+    const [weatherData, setWeatherData] = useState(null);
+    const [city, setCity] = useState("Jakarta");
+
+    useEffect(() => {
+        axios
+            .get(
+                `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${weatherKey}`
+            )
+            .then(function (response) {
+                // successful request
+                const weather = response.data;
+                setWeatherData(weather);
+            })
+            .catch(function (error) {
+                // error
+                console.log(error);
+            });
+    }, [city]);
+
+    useEffect(() => {   
+        const searchParams = history.location.search;
+        const urlParams = new URLSearchParams(searchParams);
+        const city = urlParams.get("city");
+        if (city) {
+            setCity(city);
+        }
+    }, [history]);
+
+    const { 
+        cloudiness,
+        currentTemp, 
+        highTemp, 
+        humidity,
+        lowTemp, 
+        weatherType,
+        windSpeed,
+    } = useMemo(() => {
+        let cloudiness = '';
+        let currentTemp = '';
+        let highTemp = '';
+        let humidity = '';
+        let lowTemp = '';
+        let weatherType = '';
+        let windSpeed = '';
+
+        if(weatherData) {
+            cloudiness = weatherData.clouds.all + '%';
+            currentTemp = weatherData.main.temp
+            highTemp = weatherData.main.temp_max
+            humidity = weatherData.main.humidity + '%';
+            lowTemp = weatherData.main.temp_min
+            weatherType = weatherData.weather[0].description
+            windSpeed = weatherData.wind.speed + 'km/h';
+        }
+
+        return { 
+            cloudiness,
+            currentTemp,
+            highTemp,
+            humidity,
+            lowTemp,
+            weatherType,
+            windSpeed,
+         };
+    }, [weatherData]);
+
+    // Display:
+    // Weather Type (ex. Cloudy)
+    // Current Temperature
+    // High Temperature
+    // Low Temperature
+    // Cloudiness
+    // Humidity
+    // Wind Speed  
+
+    console.log("weatherData", weatherData);
+
+    return (
+        <div>
             <Header />
             <main className="Home">
-                <h2>Weather in Jakarta</h2>
+                <h2>{city}</h2>
                 <div className="WeatherInfo">
-                    <p> Weather Type: Cloudy </p>
-                    <p> Current Temperature: 28 C </p>
-                    <p> High Temperature: 32 C </p>
-                    <p> Low Temperature: 28 C</p>
-                    <p> Cloudiness: 100 </p>
-                    <p> Humidity: 80% </p>
-                    <p> Windspeed: 5 km/h </p>
+
+                    {/* ICON */}
+                    <div className="WeatherInfo_Image">
+                        <WeatherImage weatherType={weatherType}  />
+                    </div>
+                     
+
+                    {/* WEATHER INFO */}
+                    <p className="weatherInfo_Type">{weatherType}</p>
+                    <h3 className="weatherInfo_Current">current temperature: {currentTemp}</h3>
+                    <p>high temperature: {highTemp}</p>
+                    <p>low temperature: {lowTemp}</p>
+                    <p>cloudiness: {cloudiness}</p>
+                    <p>humidity: {humidity}</p>
+                    <p>wind speed: {windSpeed}</p>
+
+
                 </div>
             </main>
-
-        </>
+        </div>
     );
 }
 
